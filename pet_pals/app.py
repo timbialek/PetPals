20c7fdfe6d43bb9b82506974361fdf5e70b62520
Tim <88064549+timbialek@users.noreply.github.com>
2022-07-04 16:21:11 -0400

    attempt8

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL2', '') #or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
